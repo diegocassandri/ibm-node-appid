f3d9ec0511210487123867b817ca4077d2534a9f
Diego Cassandri <diego.cassandri@gmail.com>
2020-09-05 12:50:44 -0300

    Implementações diversas

--- app.js
+++ app.js
@@ -1,64 +1,122 @@
 require('dotenv').config();
 const express = require('express');
-//const passport = require('passport');
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const fs = require('fs');
-//const APIStrategy = require('ibmcloud-appid').  APIStrategy;
 const app = express();
 const { v4: uuidv4 } = require('uuid');
-
-
+const axios = require('axios').default;
 app.use(express.json());
 
 const port = process.env.PORT;
 
-/*app.use(passport.initialize());
-passport.use(new APIStrategy({
-    oAuthServerUrl: "https://us-south.appid.cloud.ibm.com/oauth/v4/997a91a2-419f-4bf0-b998-03662f0c4233"
-}));
-
-app.use(passport.authenticate(APIStrategy.STRATEGY_NAME, {
-    session: false
-}));*/
+const FormData = require('form-data');
 
 app.get('/api/data', (req,res) => {
     res.json({
         data: 123
     });
 });
- 
-app.post('/login',(req,res) => {
-    const { username , password } = req.body
 
-    //Validar o usuário
+
+function validateQualitorUSer(username,password){
     if(username === 'teste' && password === 'teste'){ 
-
-        const userDataInfo = {
+        //Monto  o objeto com os dados do usuário para inserir no meu JWT
+        const userQualitorInfo = {
+            username,
             cdclient: '10',
             cdcontato: 'teste',
         }
+        return userQualitorInfo;
+    }
+}
 
-        var privateKey = fs.readFileSync(process.env.PRIVATEKEYPATH);
+function generatePrivateJWT(user){
+    //leio a chave privada para assinar o jwt
+    let privateKey = fs.readFileSync(process.env.PRIVATEKEYPATH);
+
+    //Gero o meu token JWT
+    let token =  jwt.sign({
+        exp: 100000000000,
+        iss: `${process.env.IBMAPPIDBASEURL}${process.env.TENANTID}`,
+        aud: `${process.env.IBMAPPIDBASEURL}${process.env.TENANTID}`,
+        sub: uuidv4(),
+        privateJWTInfo: user
+      }, 
+      privateKey, 
+      { 
+          algorithm: 'RS256' 
+      },
+     );
+     return token;
+}
+
+async function generateIbmJWT(privateToken){
+    var data = new FormData();
+    data.append('grant_type', 'urn:ietf:params:oauth:grant-type:jwt-bearer');
+    data.append('assertion', privateToken);
+    data.append('audience', `${process.env.IBMAPPIDBASEURL}${process.env.TENANTID}`);
+     
+
+    try {
+        var config = {
+            method: 'post',
+            url: `${process.env.IBMAPPIDBASEURL}${process.env.TENANTID}/token`, 
+            auth: {
+                username: process.env.IBMCLIENTID,
+                password: process.env.IBMSECRET
+            },
+            headers: { 
+              ...data.getHeaders()
+            },
+            data : data
+        };
+          
+        const response = await axios(config);
+
+        const ibmToken = response.data.access_token; 
+
+        return ibmToken;
+
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+async function getIbmUSerInfo(ibmToken){
+    //Uma vez ja autenticado na IBM e de posse do IBM token, busco informações do perfil do usuário
+    const responseUser = await axios.get(`${process.env.IBMAPPIDBASEURL}${process.env.TENANTID}/userInfo`,{
+        headers: {
+          'Authorization': `Bearer ${ibmToken}`
+        }
+    });
+
+    const userInfo = responseUser.data;
+
+    return userInfo;
+}
  
-        var token =  jwt.sign({
-            exp: 100000000000,
-            iss: 'https://us-south.appid.cloud.ibm.com/oauth/v4/997a91a2-419f-4bf0-b998-03662f0c4233',
-            aud: 'https://us-south.appid.cloud.ibm.com/oauth/v4/997a91a2-419f-4bf0-b998-03662f0c4233',
-            sub: uuidv4(),
-            data: { username, userDataInfo }
-          }, 
-          privateKey, 
-          { 
-              algorithm: 'RS256' 
-          },
-         );
+app.post('/login', async (req,res) => {
+    //Pega usuário e senha digitados via body
+    const { username , password } = req.body
 
+    //Validar o usuário
+    const user = validateQualitorUSer(username,password);
 
-        return res.send({ auth: true, token: token });
+    if(user){ 
+        //Gera o meu JWT
+        const privateToken = generatePrivateJWT(user);
+
+        //TOKEN IBM
+       const ibmToken = await generateIbmJWT(privateToken);
+
+       //Buscar Informações do usuário IBM
+       const userInfo = await getIbmUSerInfo(ibmToken);
+
+       return res.json({ibmToken, userInfo});
+
       }
-      
-      return res.status(500).send('Login inválido!');
-
+    
+      return res.status(500).json({ message: 'Login inválido!'});
 
 });
 
